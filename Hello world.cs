3dfb032aef16cae9c81279de4c6d8e6138ad4011
amirhosein <62234469+thiefgamer@users.noreply.github.com>
2021-11-22 18:20:27 +0330

    Add files via upload

---
+++ Hello world.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HELLO_WORLD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
+/*This is just the beginning*/
